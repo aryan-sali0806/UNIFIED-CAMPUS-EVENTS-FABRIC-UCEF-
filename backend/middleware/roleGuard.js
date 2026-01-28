bf5aa9fc8e2dc636f72242da64a82f7119b6c788
aryan-sali0806 <aryansali0808@gmail.com>
2026-01-28 19:02:29 +0530

    Added middleware

---
+++ backend/middleware/roleGuard.js
@@ -0,0 +1,22 @@
+const roleGuard = (...allowedRoles) => {
+    return (req, res, next) => {
+      if (!req.user || !req.user.role) {
+        return res.status(403).json({
+          success: false,
+          message: "Access denied",
+        });
+      }
+  
+      if (!allowedRoles.includes(req.user.role)) {
+        return res.status(403).json({
+          success: false,
+          message: "You do not have permission to perform this action",
+        });
+      }
+  
+      next();
+    };
+  };
+  
+  export default roleGuard;
+  
